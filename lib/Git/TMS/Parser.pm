4d0bf38764e1a9fe56f4daf88da682eebd943c96
George S. Baugh <george@troglodyne.net>
2024-06-24 15:10:45 -0700

    more structure

--- lib/Git/TMS/Parser.pm
+++ lib/Git/TMS/Parser.pm
@@ -6,6 +6,23 @@ use warnings;
 use 5.006;
 use v5.14.0;    # Before 5.006, v5.10.0 would not be understood.
 
+# Figure out what sub-parser we need for this programming language
+sub new {
 
+}
+
+# We need to transform raw test output + coverage tool output into the standard format of our notes.
+sub parse {
+
+}
+
+# These are expected to be overridden by child modules
+sub parse_output {
+...
+}
+
+sub parse_coverage {
+...
+}
 
 1;
